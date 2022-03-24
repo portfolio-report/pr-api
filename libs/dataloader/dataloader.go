17fe5af1327b61c3ba578d9884c6073ffc9b71da
Thomas Fabritius <tfabritius@gmail.com>
2022-03-24 21:28:37 +0100

    Implement dataloader pattern

---
+++ libs/dataloader/dataloader.go
@@ -0,0 +1,190 @@
+package dataloader
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Inspired by:
+// - https://github.com/vektah/dataloaden
+// - https://github.com/vikstrous/dataloadgen
+// - https://github.com/graph-gophers/dataloader
+
+// New creates a new Dataloader given a fetch function, maxWait, and maxSize
+func New[K comparable, V any](
+	// Fetch is a method that provides the data for the loader
+	fetch func(keys []K) ([]V, []error),
+
+	// maxWait is maximum time to wait before sending a batch
+	maxWait time.Duration,
+
+	// maxSize is maximum number of keys to send in one batch, 0 = not limit
+	maxSize int,
+) *Dataloader[K, V] {
+	return &Dataloader[K, V]{
+		fetch:   fetch,
+		maxWait: maxWait,
+		maxSize: maxSize,
+		cache:   map[K]func() (V, error){},
+	}
+}
+
+// Dataloader batches and caches requests
+type Dataloader[K comparable, V any] struct {
+	// this method provides the data for the loader
+	fetch func(keys []K) ([]V, []error)
+
+	// maximum wait time before sending batch
+	maxWait time.Duration
+
+	// maximum number of keys in batch
+	maxSize int
+
+	// cache
+	cache map[K]func() (V, error)
+
+	// the current batch. keys will continue to be collected until timeout is hit,
+	// then everything will be sent to the fetch method and out to the listeners
+	batch *batch[K, V]
+
+	// mutex to prevent races
+	mu sync.Mutex
+}
+
+type batch[K comparable, V any] struct {
+	keys    []K
+	data    []V
+	error   []error
+	closing bool
+	done    chan struct{}
+}
+
+// Load a value by key, batching and caching will be applied automatically.
+//
+// This method will block until max waiting time or max batch size is reached.
+func (l *Dataloader[K, V]) Load(key K) (V, error) {
+	return l.LoadThunk(key)()
+}
+
+// LoadThunk returns a function that when called will block waiting for a value.
+//
+// This method should be used if you want one goroutine to make requests to many
+// different data loaders without blocking until the thunk is called.
+func (l *Dataloader[K, V]) LoadThunk(key K) func() (V, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if it, ok := l.cache[key]; ok {
+		return it
+	}
+	if l.batch == nil {
+		l.batch = &batch[K, V]{done: make(chan struct{})}
+	}
+	batch := l.batch
+	pos := batch.keyIndex(l, key)
+
+	thunk := func() (V, error) {
+		<-batch.done
+
+		var data V
+
+		if len(batch.data) != len(batch.keys) {
+			return data, fmt.Errorf("bug in loader: %d values returned for %d keys", len(batch.data), len(batch.keys))
+		}
+
+		if pos < len(batch.data) {
+			data = batch.data[pos]
+		}
+
+		var err error
+		// its convenient to be able to return a single error for everything
+		if len(batch.error) == 1 {
+			err = batch.error[0]
+		} else if batch.error != nil {
+			err = batch.error[pos]
+		}
+
+		return data, err
+	}
+	l.cache[key] = thunk
+	return thunk
+}
+
+// LoadMany fetches many keys at once. It will be broken into appropriate sized
+// sub batches depending on how the loader is configured
+func (l *Dataloader[K, V]) LoadMany(keys []K) ([]V, []error) {
+	return l.LoadManyThunk(keys)()
+}
+
+// LoadManyThunk returns a function that when called will block waiting for a value.
+//
+// This method should be used if you want one goroutine to make requests to many
+// different data loaders without blocking until the thunk is called.
+func (l *Dataloader[K, V]) LoadManyThunk(keys []K) func() ([]V, []error) {
+	thunks := make([]func() (V, error), len(keys))
+	for i, key := range keys {
+		thunks[i] = l.LoadThunk(key)
+	}
+	return func() ([]V, []error) {
+		values := make([]V, len(keys))
+		errors := make([]error, len(keys))
+		noErrors := true
+		for i, thunk := range thunks {
+			values[i], errors[i] = thunk()
+			if errors[i] != nil {
+				noErrors = false
+			}
+		}
+		if noErrors {
+			return values, nil
+		}
+		return values, errors
+	}
+}
+
+// keyIndex will return the location of the key in the batch,
+// if its not found it will add the key to the batch
+func (b *batch[K, V]) keyIndex(l *Dataloader[K, V], key K) int {
+	for i, existingKey := range b.keys {
+		if key == existingKey {
+			return i
+		}
+	}
+
+	pos := len(b.keys)
+	b.keys = append(b.keys, key)
+	if pos == 0 {
+		go b.startTimer(l)
+	}
+
+	if l.maxSize != 0 && pos >= l.maxSize-1 {
+		if !b.closing {
+			b.closing = true
+			l.batch = nil
+			go b.end(l)
+		}
+	}
+
+	return pos
+}
+
+func (b *batch[K, V]) startTimer(l *Dataloader[K, V]) {
+	time.Sleep(l.maxWait)
+	l.mu.Lock()
+
+	// we must have hit a batch limit and are already finalizing this batch
+	if b.closing {
+		l.mu.Unlock()
+		return
+	}
+
+	l.batch = nil
+	l.mu.Unlock()
+
+	b.end(l)
+}
+
+func (b *batch[K, V]) end(l *Dataloader[K, V]) {
+	b.data, b.error = l.fetch(b.keys)
+	close(b.done)
+}
---
+++ libs/dataloader/dataloader_test.go
@@ -0,0 +1,250 @@
+package dataloader_test
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/portfolio-report/pr-api/libs/dataloader"
+	"github.com/stretchr/testify/require"
+)
+
+type testValue struct {
+	ID   string
+	Name string
+}
+
+// GetMockedLoader returns a dataloader and pointer to the list of
+// all calls with their keys to the mocked fetch function
+func GetMockedLoader(max int) (*dataloader.Dataloader[string, *testValue], *[][]string) {
+	var mu sync.Mutex
+	var fetchCalls [][]string
+
+	loader := dataloader.New(
+		func(keys []string) ([]*testValue, []error) {
+			mu.Lock()
+			fetchCalls = append(fetchCalls, keys)
+			mu.Unlock()
+
+			values := make([]*testValue, len(keys))
+			errors := make([]error, len(keys))
+
+			for i, key := range keys {
+				if strings.HasPrefix(key, "E") {
+					errors[i] = fmt.Errorf("not found " + key)
+				} else {
+					values[i] = &testValue{ID: key, Name: "value " + key}
+				}
+			}
+			return values, errors
+		},
+		2*time.Millisecond,
+		max,
+	)
+	return loader, &fetchCalls
+}
+
+func TestDataloader(t *testing.T) {
+	t.Run("successful Load", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		value, err := dl.Load("1")
+		require.Nil(t, err)
+		require.Equal(t, "1", value.ID)
+	})
+
+	t.Run("fetch function called only once when cached", func(t *testing.T) {
+		t.Parallel()
+		dl, fetchCallsPtr := GetMockedLoader(0)
+		for i := 0; i < 2; i++ {
+			_, err := dl.Load("E1")
+			require.Error(t, err)
+			fetchCalls := *fetchCallsPtr
+			require.Len(t, fetchCalls, 1)
+			require.Len(t, fetchCalls[0], 1)
+		}
+		for i := 0; i < 2; i++ {
+			val, err := dl.Load("U1")
+			require.Equal(t, "U1", val.ID)
+			require.NoError(t, err)
+			fetchCalls := *fetchCallsPtr
+			require.Len(t, fetchCalls, 2)
+			require.Len(t, fetchCalls[1], 1)
+		}
+	})
+
+	t.Run("failed Load", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		value, err := dl.Load("E1")
+		require.Error(t, err)
+		require.Nil(t, value)
+	})
+
+	t.Run("LoadMany", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		u, err := dl.LoadMany([]string{"U1", "U2", "E3", "E4", "U5"})
+		require.Equal(t, u[0].ID, "U1")
+		require.Equal(t, u[1].ID, "U2")
+		require.Error(t, err[2])
+		require.Error(t, err[3])
+		require.Equal(t, u[4].ID, "U5")
+	})
+
+	t.Run("LoadThunk does not contain race conditions", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		future := dl.LoadThunk("1")
+		go future()
+		go future()
+	})
+
+	t.Run("LoadThunk", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		thunk1 := dl.LoadThunk("U1")
+		thunk2 := dl.LoadThunk("E1")
+
+		u1, err1 := thunk1()
+		require.NoError(t, err1)
+		require.Equal(t, "value U1", u1.Name)
+
+		u2, err2 := thunk2()
+		require.Error(t, err2)
+		require.Nil(t, u2)
+	})
+
+	t.Run("LoadMany returns errors", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		_, err := dl.LoadMany([]string{"E1", "E2", "E3"})
+		require.Len(t, err, 3)
+	})
+
+	t.Run("LoadMany returns len(errors) == len(keys)", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(3)
+		_, errs := dl.LoadMany([]string{"E1", "U2", "U3"})
+		require.Len(t, errs, 3)
+
+		require.Error(t, errs[0])
+		require.Nil(t, errs[1])
+		require.Nil(t, errs[2])
+	})
+
+	t.Run("LoadMany returns nil []error when no errors occurred", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		_, errs := dl.LoadMany([]string{"1", "2", "3"})
+		require.Nil(t, errs)
+	})
+
+	t.Run("LoadManyThunk does not contain race conditions", func(t *testing.T) {
+		t.Parallel()
+		dl, _ := GetMockedLoader(0)
+		future := dl.LoadManyThunk([]string{"1", "2", "3"})
+		go future()
+		go future()
+	})
+
+	t.Run("requests are batched", func(t *testing.T) {
+		t.Parallel()
+		dl, fetchCallsPtr := GetMockedLoader(0)
+		future1 := dl.LoadThunk("1")
+		future2 := dl.LoadThunk("2")
+
+		_, err := future1()
+		require.Nil(t, err)
+		_, err = future2()
+		require.Nil(t, err)
+
+		fetchCalls := *fetchCallsPtr
+		require.Len(t, fetchCalls, 1)
+		require.Equal(t, []string{"1", "2"}, fetchCalls[0])
+	})
+
+	t.Run("number of results matches number of keys", func(t *testing.T) {
+		t.Parallel()
+		faultyLoader := dataloader.New(
+			func(keys []string) (results []string, errs []error) {
+				results = make([]string, len(keys)-1)
+				return results, nil
+			},
+			16*time.Millisecond,
+			0)
+
+		n := 10
+		futures := []func() (string, error){}
+		for i := 0; i < n; i++ {
+			key := strconv.Itoa(i)
+			futures = append(futures, faultyLoader.LoadThunk(key))
+		}
+
+		for _, future := range futures {
+			_, err := future()
+			require.Error(t, err)
+		}
+	})
+
+	t.Run("requests are batched with max batch size", func(t *testing.T) {
+		t.Parallel()
+		dl, fetchCallsPtr := GetMockedLoader(2)
+		future1 := dl.LoadThunk("1")
+		future2 := dl.LoadThunk("2")
+		future3 := dl.LoadThunk("3")
+
+		_, err := future1()
+		require.Nil(t, err)
+		_, err = future2()
+		require.Nil(t, err)
+		_, err = future3()
+		require.Nil(t, err)
+
+		fetchCalls := *fetchCallsPtr
+		require.Len(t, fetchCalls, 2)
+		require.Equal(t, []string{"1", "2"}, fetchCalls[0])
+		require.Equal(t, []string{"3"}, fetchCalls[1])
+	})
+
+	t.Run("repeated requests are cached", func(t *testing.T) {
+		t.Parallel()
+		dl, fetchCallsPtr := GetMockedLoader(0)
+		future1 := dl.LoadThunk("1")
+		future2 := dl.LoadThunk("1")
+
+		_, err := future1()
+		require.Nil(t, err)
+		_, err = future2()
+		require.Nil(t, err)
+
+		fetchCalls := *fetchCallsPtr
+		require.Len(t, fetchCalls, 1)
+		require.Equal(t, []string{"1"}, fetchCalls[0])
+	})
+
+	t.Run("partial fetch", func(t *testing.T) {
+		t.Parallel()
+		dl, fetchCallsPtr := GetMockedLoader(0)
+		{
+			values, errors := dl.LoadMany([]string{"U1", "E2", "U3"})
+			require.Equal(t, "U1", values[0].ID)
+			require.Error(t, errors[1])
+			require.Equal(t, "U3", values[2].ID)
+		}
+		{
+			values, errors := dl.LoadMany([]string{"E2", "U3", "E4", "U5"})
+			require.Error(t, errors[0])
+			require.Equal(t, "U3", values[1].ID)
+			require.Error(t, errors[2])
+			require.Equal(t, "U5", values[3].ID)
+		}
+
+		fetchCalls := *fetchCallsPtr
+		require.Len(t, fetchCalls, 2)
+		require.Len(t, fetchCalls[1], 2) // Second fetch call only retrieves additional keys E4 and U5
+	})
+}
