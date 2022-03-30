c51727cf5c878f87f627730808f1d0fb6c1762b5
Thomas Fabritius <tfabritius@gmail.com>
2022-03-30 21:51:41 +0200

    Test statistics on client updates

--- main_test.go
+++ main_test.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/portfolio-report/pr-api/db"
@@ -410,4 +412,100 @@ func TestTaxonomies(t *testing.T) {
 
 		a.Equal(200, res.Code)
 	}
+}
+
+func TestStats(t *testing.T) {
+	handlerConfig.DB.Model(&db.User{}).Where("username = 'testuser-e2e'").Update("is_admin", true)
+	handlerConfig.DB.Debug().Delete(&db.Clientupdate{}, "version = 'test-version'")
+
+	a := assert.New(t)
+
+	// HEAD request
+	{
+		res := req("HEAD", "/stats/update/name.abuchen.portfolio/test-version", nil)
+		a.Equal(http.StatusOK, res.Code)
+		a.Equal(res.Body.Len(), 0)
+	}
+
+	// GET /updates
+	{
+		res := req("GET", "/stats/updates", nil)
+		a.Equal(http.StatusOK, res.Code)
+		var body []gin.H
+		err := json.Unmarshal(res.Body.Bytes(), &body)
+		a.Nil(err)
+		a.GreaterOrEqual(len(body), 1)
+
+		found := false
+		for _, b := range body {
+			if b["version"] == "test-version" {
+				found = true
+				a.Equal(b["count"], 1.)
+				_, err = time.Parse("2006-01-02T15:04:05Z07:00", b["firstUpdate"].(string))
+				a.Nil(err)
+				timestamp, err := time.Parse("2006-01-02T15:04:05Z07:00", b["lastUpdate"].(string))
+				a.Nil(err)
+				age := time.Now().Sub(timestamp)
+				a.Positive(age)
+				a.Less(age, 1*time.Second)
+			}
+		}
+		a.True(found)
+	}
+
+	// GET /update/test-version
+	{
+		res := req("GET", "/stats/updates/test-version", nil)
+		a.Equal(http.StatusOK, res.Code)
+		var body gin.H
+		err := json.Unmarshal(res.Body.Bytes(), &body)
+		a.Nil(err)
+		a.NotNil(body["byDate"])
+		a.NotNil(body["byCountry"])
+	}
+
+	var id int
+	// GET all updates
+	{
+		req := httptest.NewRequest("GET", "/stats/?version=test-version", nil)
+		req.Header.Add("Authorization", "Bearer "+session.Token)
+		res := httptest.NewRecorder()
+		app.ServeHTTP(res, req)
+
+		a.Equal(http.StatusOK, res.Code)
+		var body gin.H
+		err := json.Unmarshal(res.Body.Bytes(), &body)
+		a.Nil(err)
+		a.NotNil(body["entries"])
+		a.NotNil(body["params"])
+
+		entries, ok := body["entries"].([]interface{})
+		a.True(ok)
+		a.Len(entries, 1)
+
+		entry, ok := entries[0].(map[string]interface{})
+		a.True(ok)
+		a.Equal("test-version", entry["version"])
+		a.True(entry["country"] == "-" || entry["country"] == "")
+		a.Equal("", entry["useragent"])
+		id = int(entry["id"].(float64))
+
+		timestamp, err := time.Parse("2006-01-02T15:04:05Z07:00", entry["timestamp"].(string))
+		a.Nil(err)
+		age := time.Now().Sub(timestamp)
+		a.Positive(age)
+		a.Less(age, 1*time.Second)
+	}
+
+	// DELETE update
+	{
+		req := httptest.NewRequest("DELETE", "/stats/"+strconv.Itoa(id), nil)
+		req.Header.Add("Authorization", "Bearer "+session.Token)
+		res := httptest.NewRecorder()
+		app.ServeHTTP(res, req)
+
+		a.Equal(http.StatusNoContent, res.Code)
+		a.Equal(0, res.Body.Len())
+	}
+
 }
